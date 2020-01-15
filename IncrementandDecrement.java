5365a552e5e98b6cc6c5e0dbf5b49b2875626924
Alexander <36586728+alexander941@users.noreply.github.com>
2020-01-15 11:13:04 +0300

    Add files via upload

---
+++ IncrementandDecrement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication4;
+
+/**
+ *
+ * @author Alex Shustov
+ */
+public class IncrementandDecrement {
+    public static void main (String[]args){
+        for(int ff=5; ff<50; ff++){
+            System.out.println(ff);
+        }
+    }
+}
