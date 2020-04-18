d1823027d028a10780d1f42a0bdd33db9740927d
Alexander <36586728+alexander941@users.noreply.github.com>
2020-04-19 00:19:16 +0300

    Add files via upload

---
+++ OOPInheritance/Animals.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaprog;
+
+/**
+ *
+ * @author Alex Shsutov 
+ * Наследование ООП
+ */
+public class Animals { //Главный класс
+       static int age;
+       static double height;
+       static double weight;
+       static String color;
+       public void eat(){
+          System.out.println("Ем");
+       }
+       public void drink(){
+            System.out.println("Пью");
+       }
+       public void sleep(){
+           System.out.println("Сплю");
+       }
+       
+
+}
---
+++ OOPInheritance/MainInheritance.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaprog;
+
+/**
+ * Наследование ООП
+ * @author Alex Shustov 
+ */
+public class MainInheritance {
+    public static void main(String[]args){
+        Animals Ani = new Animals();
+        
+        
+        Сat  Ca = new Сat ();
+        Ca.eat();
+        Ca.drink();
+        Ca.sleep();
+        
+    }
+}
